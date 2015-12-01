376502f51a25f034c35133b376dc6b2bc25e9930
resteve <resteve@zikzakmedia.com>
2015-12-01 10:58:16 +0100

    Tests

--- tests/test_account_invoice_purchase_relation.py
+++ tests/test_account_invoice_purchase_relation.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
+# This file is part of the account_invoice_purchase_relation module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoicePurchaseRelationTestCase(unittest.TestCase):
+class AccountInvoicePurchaseRelationTestCase(ModuleTestCase):
     'Test Account Invoice Purchase Relation module'
-
-    def setUp(self):
-        module = 'account_invoice_purchase_relation'
-        trytond.tests.test_tryton.install_module(module)
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_purchase_relation'
 
 
 def suite():
@@ -23,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoicePurchaseRelationTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
